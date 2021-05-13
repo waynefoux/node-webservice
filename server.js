412bee91ec7d89a9e338f01ee99f4ec5e4a15d73
Wayne Foux <wayne.foux@gmail.com>
2021-05-13 19:19:14 -0400

    Incorperate Redis

--- server.js
+++ server.js
@@ -6,12 +6,46 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+// Redis
+const redis = require('redis');
+const client = redis.createClient({
+    //host: process.env.REDIS_HOST,
+    host: 'redis',
+    port: 6379,
+    //password: process.env.REDIS_PASSWORD,
+});
+
+client.on('error', err => {
+  console.log('Error ' + err);
+});
+
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(express.json());
+
+// GET
+app.get('/:key', (req, res) => {
+
+  client.get(req.params.key, (err, reply) => {
+    if (err) throw err;
+      console.log(reply);
+
+      return res.send(reply)
+      .status(200);
+  });
+});
+
+// POST
+app.post('/', (req, res) => {
+
+  client.set(req.body.key, req.body.value, (err, reply) => {
+    if (err) throw err;
+    console.log(reply);
+    
+    res.send(req.body)
+    return res.status(200);
+  });
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
